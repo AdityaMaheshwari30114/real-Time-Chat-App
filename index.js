2d682331d12cb33626d57ffb462801e843045322
AdityaMaheshwari30114 <adityamaheshwari30114@gmail.com>
2025-06-04 20:30:39 +0530

    added broadcast & nickname functionality

--- index.js
+++ index.js
@@ -8,12 +8,32 @@ const server = http.createServer(app); ////server is a real HTTP server
 const io = new Server(server);      //io is your Socket.IO server attached to the HTTP server,
                                     //which will handle WebSocket communication with clients.
 
+const users={};
 // Socket.io Setup
+
 io.on('connection', (socket) =>{
+    console.log("A user Connected:", socket.id);
+
+    // When user sends nickname
+    socket.on('set-nickname', nickname =>{
+        users[socket.id] = nickname;
+    socket.broadcast.emit('message', `${nickname} joined the chat`);
+    });
+
+    // Handle chat messages
     socket.on('user-message', message =>{
-        io.emit('message', message);
+        const nickname =users[socket.id] ||'Anonymous';
+        io.emit('message', `${nickname}: ${message}`);
+    });
+
+    // Handle disconnect
+    socket.on('disconnect', ()=>{
+    const nickname =users[socket.id] || 'Someone';
+    socket.broadcast.emit('message', `${nickname} left the chat`);
+    delete users[socket.id];
     })
-})
+});
+
 
 app.use(express.static(path.resolve("./public")));
 app.get('/', (req,res) => {
